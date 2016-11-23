9eff35144449643327e264863dcd1574064b5626
milos01 <milosa942@gmail.com>
2016-11-23 22:53:57 +0100

    Created database, User and Application models and  rest endpoints for listing and adding Users

---
+++ model/application.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// kreiramo novu shemu
+
+var ApplicaitionSchema = new Schema({
+  app_name: {
+    type: String,
+    required: true
+  },
+  createdAt: Date,
+  updatedAt: Date,
+});
+
+ApplicaitionSchema.pre('save', function(next) {
+  // preuzmemo trenutni datum
+  var currentDate = new Date();
+
+  // postavimo trenutni datum poslednju izmenu
+  this.updatedAt = currentDate;
+
+  // ako nije postavljena vrednost za createdAt, postavimo je
+  if (!this.createdAt)
+    this.createdAt = currentDate;
+
+  // predjemo na sledecu funckiju u lancu
+  next();
+});
+
+// od sheme kreiramo model koji cemo koristiti
+
+
+// publikujemo kreirani model
+module.exports = ApplicaitionSchema;
---
+++ model/user.js
@@ -0,0 +1,51 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var application = require('./application');
+// kreiramo novu shemu
+
+
+var UsersSchema = new Schema({
+  first_name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+    // unique: true
+  },
+  createdAt: Date,
+  updatedAt: Date,
+  // napomena! komentari su u ovom primeru implementirani kao reference zbog ilustracije rada sa referencama
+  // u realnom sluacju bolje bi bilo implementirati ih kao poddokumente
+  applications: [application]
+});
+
+
+
+
+// prilikom snimanja se postavi datum
+UsersSchema.pre('save', function(next) {
+  // preuzmemo trenutni datum
+  var currentDate = new Date();
+
+  // postavimo trenutni datum poslednju izmenu
+  this.updatedAt = currentDate;
+
+  // ako nije postavljena vrednost za createdAt, postavimo je
+  if (!this.createdAt)
+    this.createdAt = currentDate;
+
+  // predjemo na sledecu funckiju u lancu
+  next();
+});
+
+// od sheme kreiramo model koji cemo koristiti
+var User = mongoose.model('User', UsersSchema);
+
+// publikujemo kreirani model
+module.exports = User;
